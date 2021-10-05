7fb3bb440786752385b3cb88bb2365551e61ae90
RemyDy <regcschut@gmail.com>
2021-10-05 17:26:26 +0200

    loops gemaakt

--- loops.js
+++ loops.js
@@ -6,7 +6,9 @@
 // Hoera!
 // ==========================================
 
-
+for (i = 0; i < 3; i++){
+    console.log("Hoera!");
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -16,9 +18,15 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
+// ==========================================
 
-
+for (i = 0; i < 5; i++){
+    if (i === 4){
+        console.log("klaar");
+    } else {
+        console.log("loop");
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -32,7 +40,15 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
+//
 
+for (i = 0; i < 16; i++){
+   if (i % 2){
+       console.log(i + " is even");
+   } else {
+       console.log(i + " is oneven");
+   }
+}
 
 
 // ==========================================
@@ -51,3 +67,12 @@
 // >> 9
 // ==========================================
 
+for (i = 0; i < 10; i++) {
+    if (i < 3) {
+    console.log(i);
+    } else if (i > 5){
+    console.log(">> " + i);
+    } else {
+        console.log("> " + i);
+    }
+}
